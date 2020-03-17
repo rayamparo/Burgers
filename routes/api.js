b401f23457c54c58931a68786b563065e22cf6dd
Raymond Amparo <ray.amparo1995@gmail.com>
2020-03-16 17:24:59 -0700

    changes made on routes

--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,33 @@
-// let express = require('express');
-// let burger = require('../models/burger');
+let express = require('express');
+let burger = require('../models/burger');
 
-// let router = express.Router();
+let router = express.Router();
 
-// router.get("/burgers", (req, res) => {
-//   burger.all((data) => {
-//     res.json(data);
-//   })
-// }); 
+router.get("/burgers", (req, res) => {
+  burger.all((data) => {
+    res.json(data);
+  })
+}); 
 
-// router.post("/burgers", function(req, res) {
-//   burger.create([
-//     "name"
-//   ], [
-//     req.body.name
-//   ], function(result) {
-//     // Send back the ID of the new quote
-//     res.json({ id: result.insertId });
-//   });
-// });
+router.post("/burgers", function(req, res) {
+  burger.create([
+    "name"
+  ], [
+    req.body.name
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
 
-// router.put('/burgers/:id', (req, res) => {
-//   burger.update({ devoured: true }, `id = ${req.params.id}`, (result) => {
-//     if(result.changedRows == 0) {
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
+router.put('/burgers/:id', (req, res) => {
+  burger.update({ devoured: true }, `id = ${req.params.id}`, (result) => {
+    if(result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
-// module.exports = router;
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-// const express = require('express');
-// const burger = require('../models/burger');
+const express = require('express');
+const burger = require('../models/burger');
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.get('/', (req, res) => {
-//   burger.all((data) => {
-//     const ejsObj = {
-//       burgers: data
-//     };
-//     res.render('burgers', ejsObj);
-//   })
-// }); 
+router.get('/', (req, res) => {
+  burger.all((data) => {
+    const ejsObj = {
+      burgers: data
+    };
+    res.render('burgers', ejsObj);
+  })
+}); 
 
-// module.exports = router;
+module.exports = router;
